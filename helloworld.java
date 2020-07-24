6bb8252525423848c161effe97bcf6637b54f3ed
uday pspk <udaykumar90599@gmail.com>
2020-07-24 12:50:35 +0530

    gng to fight

--- helloworld.java
+++ helloworld.java
@@ -4,7 +4,8 @@ public class HelloWorld
      public static void main(String[] args)
             {
                 System.out.println("Hello to Terminal Command");
-               echo uday   
+               echo "u0000ay"
           }
 
 }
+
