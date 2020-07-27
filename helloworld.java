80aa472d0a70b8c2b86445c59e552167294440f8
uday pspk <udaykumar90599@gmail.com>
2020-07-27 11:30:57 +0530

    master branch updation

--- helloworld.java
+++ helloworld.java
@@ -4,7 +4,7 @@ public class HelloWorld
      public static void main(String[] args)
             {
 
-                System.out.println("Hello to Terminal  udatyCommand");
+                System.out.println("this is my master branch");
                echo uday
 
           }
